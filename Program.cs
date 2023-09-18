0d3341d4b173b129b7cb2b8b293f8c37782e86d5
Kevin <129830470+FireAce3000@users.noreply.github.com>
2023-09-18 13:31:21 +0000

    C# mit "dotnet run" ausgeführbar

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("erstes Projekt");
+            Console.WriteLine("first project");
+
+            Console.Write("Bitte geben Sie Ihr Alter ein: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int alter))
+            {
+                if (alter >= 18)
+                {
+                    Console.WriteLine("Sie sind 18 Jahre oder älter. / You are 18 years or older");
+                }
+                else
+                {
+                    Console.WriteLine("Sie sind jünger als 18 Jahre. / You are yunger than 18 years old");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ungültige Eingabe. Bitte geben Sie eine ganze Zahl für Ihr Alter ein. / Invalid input. Pleas enter a qhole number for your age.");
+            }
+        
+        }
+    }
+}
--- class.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public class class
-{
-    public static void Main(string[] args)
-    {
-        if(Math.)
-    }
-}
