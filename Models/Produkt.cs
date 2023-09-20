23adaac18b848c7ea4dbd2c8f104f41ed005e936
Kevin <129830470+FireAce3000@users.noreply.github.com>
2023-09-20 07:38:45 +0000

    DatenVerwaltung

---
+++ Models/Produkt.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Produkt
+    {
+        public int ProduktId { get; set; }
+        public string ProduktName { get; set; }
+        public string Beschreibung { get; set; }
+    }
+}
