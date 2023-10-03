87d5004075f5990860cd8a165e50b7e48fa1131c
Kevin <129830470+FireAce3000@users.noreply.github.com>
2023-10-03 06:50:37 +0000

    add func case 9: definition

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Security.Cryptography;
 using ERSTES;
 using System.Threading.Channels;
+using System.Reflection.Metadata;
 
 namespace ERSTES
 {
@@ -26,6 +27,7 @@ namespace ERSTES
                 Console.WriteLine("6 - LADEN - Aus der Datei laden");
                 Console.WriteLine("7 - SPEICHERN - In die Datei speichern");
                 Console.WriteLine("8 - LEEREN - Aktuelle Liste leeren");
+                Console.WriteLine("9 - DEFINITION - alle Daten mit Liter ausgeben");
                 Console.WriteLine();
                 Console.Write("AUSWAHL: ");
 
@@ -74,188 +76,128 @@ namespace ERSTES
                             ProduktManager.ClearProdukte();
                             Pause("Liste der Personen wurde geleert.");
                             break;
+                        case 9:
+                            // DEFINITION - alle Daten mit Liter ausgeben
+                            Definition();
+                            Pause("Daten wurden gefunden");
+                            break;
                         default:
                             Pause("Ungültige Auswahl. Bitte erneut wählen.");
                             break;
                     }
                 }
-
                 else
-
                 {
-
                     Pause("Bitte geben Sie eine gültige Nummer ein.");
-
                     auswahl = -1; // Durchlauf wiederholen
-
                 }
-
             } while (auswahl != 0);
+        }
 
+        private static void Definition()
+        {
+            var definition = ProduktManager.Definition();
+            ProduktAusgeben(definition);
+        }
+
+        private static void ProduktAusgeben(List<Produkt> produktListe)
+        {
+            foreach (var p in produktListe)
+            {
+                Console.WriteLine($"ID: {p.ProduktId}, Produktname: {p.ProduktName}, Beschreibung: {p.Beschreibung}");
+            }
         }
 
         private static void Pause(string message = "")
-
         {
-
             Console.WriteLine();
-
             if (message != string.Empty) Console.WriteLine(message + "\n");
-
             Console.WriteLine("--- Weiter mit ENTER ---");
-
             Console.ReadLine();
-
         }
 
         static void ListAllProdukte()
-
         {
-
             Console.WriteLine(" *** Alle Einträge ***");
-
             Console.WriteLine();
-
             var produkte = ProduktManager.GetAllProdukte();
 
             foreach (var produkt in produkte)
-
             {
-
                 Console.WriteLine($"ID: {produkt.ProduktId}, Produktname: {produkt.ProduktName}, Beschreibung: {produkt.Beschreibung}");
-
             }
-
             Pause();
-
         }
 
         static void CreateNewProdukt()
-
         {
-
             Console.WriteLine(" *** Neuer Eintrag *** ");
-
             Console.WriteLine();
-
             Console.WriteLine("Bitte geben Sie den Produktnamen ein:");
-
             var produktName = Console.ReadLine();
 
             Console.WriteLine("Bitte geben Sie die Beschreibung ein:");
-
             var beschreibung = Console.ReadLine();
 
             var produkt = new Produkt { ProduktName = produktName, Beschreibung = beschreibung };
-
             // Hier könnte eine ID-Generierungsfunktion implementiert werden
-
             ProduktManager.AddProdukt(produkt);
-
             Pause("Produkt erfolgreich hinzugefügt.");
-
         }
 
         static void UpdateProdukt()
-
         {
-
             Console.WriteLine(" *** Eintrag Aktualisieren *** ");
-
             Console.WriteLine();
-
             Console.WriteLine("Bitte geben Sie die ID der zu bearbeitenden Produkt ein:");
 
             if (int.TryParse(Console.ReadLine(), out int id))
-
             {
-
                 var produkt = ProduktManager.GetProduktById(id);
-
                 if (produkt != null)
-
                 {
-
                     Console.WriteLine($"Aktuelle Daten - Produktname: {produkt.ProduktName}, Beschreibung: {produkt.Beschreibung}");
-
                     Console.WriteLine("Bitte geben Sie den neuen Produktnamen ein:");
-
                     var produktNameNeu = Console.ReadLine();
-
                     Console.WriteLine("Bitte geben Sie die neue Beschreibung ein:");
-
                     var beschreibungNeu = Console.ReadLine();
-
                     ProduktManager.UpdateProdukt(id, produktNameNeu, beschreibungNeu);
-
                     Console.WriteLine("Produkt erfolgreich aktualisiert.");
-
                 }
-
                 else
-
                 {
-
                     Console.WriteLine("Keine Produkt mit dieser ID gefunden.");
-
                 }
-
             }
-
             else
-
             {
-
                 Console.WriteLine("Ungültige ID.");
-
             }
-
         }
 
         static void DeleteProdukt()
-
         {
-
             Console.WriteLine(" *** Eintrag Löschen *** ");
-
             Console.WriteLine();
-
             Console.WriteLine("Bitte geben Sie die ID des zu löschenden Produktes ein:");
 
             if (int.TryParse(Console.ReadLine(), out int id))
-
             {
-
                 var produkt = ProduktManager.GetProduktById(id);
-
                 if (produkt != null)
-
                 {
-
                     ProduktManager.DeleteProdukt(id);
-
                     Console.WriteLine("Produkt erfolgreich gelöscht.");
-
                 }
-
                 else
-
                 {
-
                     Console.WriteLine("Kein Produkt mit dieser ID gefunden.");
-
                 }
-
             }
-
             else
-
             {
-
                 Console.WriteLine("Ungültige ID.");
-
             }
-
         }
     }
 }
